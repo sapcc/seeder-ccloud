801ca1de099df78d7afc0b687a3ebbefd8c76f40
Stefan Hipfel <stefan.hipfel@sap.com>
2022-02-04 10:31:08 +0100

    adds exporter within seeder_ccloud module

--- seeder_ccloud/exporter/seeds_exporter.py
+++ seeder_ccloud/exporter/seeds_exporter.py
@@ -1,10 +1,11 @@
+import sys
 import kopf
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 from kubernetes import client, config
 from time import sleep
 from kopf._cogs.structs import bodies
-from seeder_ccloud.seeder_operator import PREFIX, SEED_CRD
+from seeder_ccloud import utils
 import logging
 
 try:
@@ -12,6 +13,11 @@ try:
 except config.ConfigException:
     config.load_incluster_config()
 
+try:
+    config = utils.Config()
+except Exception as e:
+    print(e)
+    sys.Exit(0)
 
 class SeedsCollector(object):
     def describe(self):
@@ -23,14 +29,14 @@ class SeedsCollector(object):
         status = GaugeMetricFamily('ccloud_seeds_status', 'Shows the status of a single seed', labels=['name'])
         api = client.CustomObjectsApi()
         seeds = api.list_cluster_custom_object(
-            group=SEED_CRD['group'], 
-            version=SEED_CRD['version'],
-            plural=SEED_CRD['plural'],
+            group=config.crd_info['group'], 
+            version=config.crd_info['version'],
+            plural=config.crd_info['plural'],
         )
         total.add_metric(labels=[], value=len(seeds['items']))
         yield total
         storage = kopf.AnnotationsDiffBaseStorage(
-            prefix=PREFIX,
+            prefix=config.prefix,
             key='last-handled-configuration',
         )
         for seed in seeds['items']:
@@ -54,8 +60,12 @@ class SeedsCollector(object):
 REGISTRY.register(SeedsCollector())
 
 
-if __name__ == '__main__':
+def main():
     # Start up the server to expose the metrics.
-    start_http_server(8000)
+    start_http_server(9000)
     while True:
         sleep(10)
+
+
+if __name__ == '__main__':
+    main()
